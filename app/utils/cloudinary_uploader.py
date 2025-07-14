0484082177905f20a698e074d008296ab6b5e89c
swayam satyabrat das <swayam.hirethen@gmail.com>
2025-07-14 10:54:05 +0530

    update cloudinary page

--- app/utils/cloudinary_uploader.py
+++ app/utils/cloudinary_uploader.py
@@ -1,12 +1,18 @@
+import os
+from dotenv import load_dotenv
 import cloudinary
 import cloudinary.uploader
 
+
+load_dotenv()
+
+
 cloudinary.config(
-  cloud_name='dh2h6ia0n',
-  api_key='853734915947643',
-  api_secret='X9p-pAu7ZNjDnQEnx0_HMFuLl0o'
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
 )
 
 def upload_to_cloudinary(file, folder='user'):
-    result = cloudinary.uploader.upload(file, folder=folder,resource_type="auto")
+    result = cloudinary.uploader.upload(file, folder=folder, resource_type="auto")
     return result['secure_url']
